186ea7aecdf8a38332e90908f968321bf9b2c3db
Farhad Esmaeili <farhadesmzdh@gmail.com>
2024-08-07 18:15:55 +0330

    feat: update deploy.sh to run docker compose file

--- notes/deploy.sh
+++ notes/deploy.sh
@@ -4,6 +4,8 @@ IMAGE_NAME="notes"
 DOCKERFILE_PATH="."
 TAG="latest"
 
+COMPOSE_FILE="docker-compose.yml"
+
 echo "Building Docker image '$IMAGE_NAME:$TAG' from '$DOCKERFILE_PATH/Dockerfile'..."
 
 docker build -t "$IMAGE_NAME:$TAG" "$DOCKERFILE_PATH"
@@ -12,5 +14,15 @@ if [ $? -eq 0 ]; then
     echo "Docker image '$IMAGE_NAME:$TAG' built successfully."
 else
     echo "Error: Docker image build failed."
+    exit 1
+fi
+
+echo "Running Docker Compose using file '$COMPOSE_FILE'..."
+docker compose -f "$COMPOSE_FILE" up -d
+
+if [ $? -eq 0 ]; then
+    echo "Docker Compose ran successfully."
+else
+    echo "Error: Docker Compose failed."
     exit 1
 fi
